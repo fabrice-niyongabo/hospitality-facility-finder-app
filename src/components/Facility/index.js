95feb7eb6e161ac4a3e4aa3f64e8232f40420f58
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-05-09 07:14:59 +0200

    facility details

--- src/components/Facility/index.js
+++ src/components/Facility/index.js
@@ -1,7 +1,17 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../Header";
+import PlaceHolder from "./PlaceHolder";
 function Facility() {
-  return <div>Facility</div>;
+  const { id } = useParams();
+  const [isLoading, setisLoading] = useState(true);
+  const [facility, setFacility] = useState(null);
+  return (
+    <>
+      <Header />
+      {isLoading ? <PlaceHolder /> : <></>}
+    </>
+  );
 }
 
 export default Facility;
